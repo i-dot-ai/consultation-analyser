61f14cc0420d27d2f60edd8eaa23ab4a3e4f96bc
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-03-25 15:25:02 +0000

    tidy

--- tests/factories.py
+++ tests/factories.py
@@ -117,5 +117,4 @@ class AnswerFactory(factory.django.DjangoModelFactory):
 
     question = factory.SubFactory(QuestionFactory)
     consultation_response = factory.SubFactory(ConsultationResponseFactory)
-
     theme = factory.LazyAttribute(lambda o: ThemeFactory() if o.free_text else None)
